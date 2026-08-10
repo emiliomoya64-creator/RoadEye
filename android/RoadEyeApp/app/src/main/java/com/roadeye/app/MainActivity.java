34772c2e4a7eb20eba19361c4d4c696c492db48b
Emilio Moya <emiliomoya64@gmail.com>
2026-08-10 20:48:39 +0200

    RoadEye Android: fullscreen inmersivo y boton reposicionado

--- android/RoadEyeApp/app/src/main/java/com/roadeye/app/MainActivity.java
+++ android/RoadEyeApp/app/src/main/java/com/roadeye/app/MainActivity.java
@@ -258,7 +258,18 @@ public class MainActivity extends Activity {
                         display: none !important;
                     }
 
-                    .dashboard,
+                    main.dashboard {
+                        display: block !important;
+                    }
+
+                    .dashboard > * {
+                        display: none !important;
+                    }
+
+                    .dashboard > .camera-panel {
+                        display: block !important;
+                    }
+
                     .camera-panel,
                     .video-frame,
                     #videoStream {
@@ -372,6 +383,17 @@ public class MainActivity extends Activity {
 
 
     private void hideSystemBars() {
+
+        getWindow().getDecorView()
+            .setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+
         if (
             android.os.Build.VERSION.SDK_INT
             >= android.os.Build.VERSION_CODES.R
@@ -381,7 +403,8 @@ public class MainActivity extends Activity {
 
             if (controller != null) {
                 controller.hide(
-                    WindowInsets.Type.systemBars()
+                    WindowInsets.Type.statusBars()
+                    | WindowInsets.Type.navigationBars()
                 );
 
                 controller.setSystemBarsBehavior(
@@ -389,13 +412,6 @@ public class MainActivity extends Activity {
                         .BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
                 );
             }
-        } else {
-            getWindow().getDecorView()
-                .setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                );
         }
     }
 
